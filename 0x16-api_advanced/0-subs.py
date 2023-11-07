0d676e6faa74a712200551436d18321e111e9a20
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-11-07 15:17:56 +0000

    task answer

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Returns the number of subscribers in given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the subscribers """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'CustomUserAgent'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return (data['data']['subscribers'])
+    else:
+        return (0)
