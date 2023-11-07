1627af97db235695d813715aa4874c5a109951f4
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-11-07 15:18:55 +0000

    task answer

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'CustomUserAgent'}
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         return (data['data']['subscribers'])
