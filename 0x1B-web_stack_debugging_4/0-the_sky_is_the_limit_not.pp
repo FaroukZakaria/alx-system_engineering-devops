0356e1e125288e4c16725fa8c863a13e0b0c9d23
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2023-11-17 16:59:58 +0200

    task answer

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Increases the limit of Nginx traffic
+# Increases the default's ULIMIT
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
