910677329eb3c7cb51404376547c6c34badd7770
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2023-11-17 17:04:55 +0200

    nevermind... puppet lint fails again

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Increases the limit of Nginx traffic
 # Increases the default's ULIMIT
-exec { 'fix-for-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin'],
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => ['/etc/init.d'],
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
